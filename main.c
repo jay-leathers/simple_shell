3f640d8541f8a3551819a606ec5539c50bdf182c
Ikenna Nwachukwu <ikezudon@gmail.com>
2022-08-11 15:39:57 -0700

    fixed main.c file

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-File Edit Options Buffers Tools C Help
 #include "main.h"
-
 
 void sig_handler(int sig);
 int execute(char **args, char **front);
@@ -9,159 +7,72 @@ int execute(char **args, char **front);
  * sig_handler - Prints a new prompt upon a signal.
  * @sig: The signal.
  */
-
 void sig_handler(int sig)
-
 {
-
   char *new_prompt = "\n$ ";
 
-
-
   (void)sig;
-
-  -UU-:----F1  main.c         Top L1    Git-main  (C/*l Abbrev) ------------------
-For information about GNU Emacs and the GNU system, type C-h C-a.
-#include "main.h"
-
-
-void sig_handler(int sig);
-int execute(char **args, char **front);
-
-/**
- * sig_handler - Prints a new prompt upon a signal.
- * @sig: The signal.
- */
-
-void sig_handler(int sig)
-
-{
-
-  char *new_prompt = "\n$ ";
-
-
-
-  (void)sig;
-
   signal(SIGINT, sig_handler);
-
   write(STDIN_FILENO, new_prompt, 3);
-
 }
 
-
-
 /**
-
  * execute - Executes a command in a child process.
-
  * @args: An array of arguments.
-
  * @front: A double pointer to the beginning of args.
-
  *
-
  * Return: If an error occurs - a corresponding error code.
-
  *         O/w - The exit value of the last executed command.
-
  */
-
 int execute(char **args, char **front)
-
 {
-
   pid_t child_pid;
-
   int status, flag = 0, ret = 0;
-
   char *command = args[0];
 
-
-
   if (command[0] != '/' && command[0] != '.')
-
-    {
-
+  {
       flag = 1;
-
       command = get_location(command);
-
-    }
-
-
+  }
 
   if (!command || (access(command, F_OK) == -1))
-
-    {
-
+  {
       if (errno == EACCES)
-
 	ret = (create_error(args, 126));
-
       else
-
 	ret = (create_error(args, 127));
-
-    }
-
+  }
   else
-
-    {
-
+  {
       child_pid = fork();
-
       if (child_pid == -1)
-
-	{
-
+      {
 	  if (flag)
-
 	    free(command);
-
 	  perror("Error child:");
-
 	  return (1);
-
-	}
-
+      }
       if (child_pid == 0)
-
-	{
-
+      {
 	  execve(command, args, environ);
-
 	  if (errno == EACCES)
-
 	    ret = (create_error(args, 126));
-
 	  free_env();
-
 	  free_args(args, front);
-
 	  free_alias_list(aliases);
-
 	  _exit(ret);
-
-	}
-
+      }
       else
-
-	{
-
+      {
 	  wait(&status);
-
 	  ret = WEXITSTATUS(status);
-
-	}
-
-    }
-
+      }
+  }
   if (flag)
     free(command);
   return (ret);
 }
-
 
 /**
  * main - Runs a simple UNIX command interpreter.
@@ -172,7 +83,6 @@ int execute(char **args, char **front)
  */
 
 int main(int argc, char *argv[])
-
 {
   int ret = 0, retn;
   int *exe_ret = &retn;
@@ -189,35 +99,35 @@ int main(int argc, char *argv[])
     exit(-100);
 
   if (argc != 1)
-    {
+  {
       ret = proc_file_commands(argv[1], exe_ret);
       free_env();
       free_alias_list(aliases);
       return (*exe_ret);
-    }
+  }
 
   if (!isatty(STDIN_FILENO))
-    {
+  {
       while (ret != END_OF_FILE && ret != EXIT)
 	ret = handle_args(exe_ret);
       free_env();
       free_alias_list(aliases);
       return (*exe_ret);
-    }
+  }
 
   while (1)
-    {
+  {
       write(STDOUT_FILENO, prompt, 2);
       ret = handle_args(exe_ret);
       if (ret == END_OF_FILE || ret == EXIT)
-	{
+      {
 	  if (ret == END_OF_FILE)
 	    write(STDOUT_FILENO, new_line, 1);
 	  free_env();
 	  free_alias_list(aliases);
 	  exit(*exe_ret);
-	}
-    }
+      }
+  }
 
   free_env();
   free_alias_list(aliases);
